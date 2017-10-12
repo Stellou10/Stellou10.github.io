ea38cff4c210e3c2ff214672a83b0729ce38e5dc
Stellou10 <32701601+Stellou10@users.noreply.github.com>
2017-10-12 10:09:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hello");
+alert("hi!");
+
+document.getElementById('pop').innerHTML = "I put text here!";
+
+console.log(0.2345*1000);
+console.log(2+5);
+console.log(7/8);
+console.log(Math.random()*100);
+
